44190610743f48d99ce70e649935c790d3b3449a
Brandon Michaud <brandondmichaud@ou.edu>
2022-09-09 23:06:53 +0000

    Add functionality for non-integers

--- a0.c
+++ a0.c
@@ -5,8 +5,11 @@ int main(int argc, char** arg, char** envp) {
 	// Declare an integer to hold input from stdin	
 	int val;
 
+	// Declare an integer and assign the return value of the scan to it
+	int status = scanf("%d", &val);
+
 	// Read input until the EOF token or non-integer
-	while(scanf("%d", &val) != EOF) {
+	while(status != 0 && status != EOF) {
 
 		// Check if the value is divisible by 3
 		if (val % 3 == 0) {
@@ -33,7 +36,7 @@ int main(int argc, char** arg, char** envp) {
 			// If the value is not divisble by either 3 or 5 print out the value
 			printf("%d\n", val);
 		}
-
+		status = scanf("%d", &val);
 	}	
 	// Default return
 	return 0;
