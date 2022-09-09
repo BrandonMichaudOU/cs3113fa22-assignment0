3b8546ebce414f8dbbc6efa1a87dae7d4c1e2365
Brandon Michaud <brandondmichaud@ou.edu>
2022-09-09 22:52:29 +0000

    Add main function

---
+++ a0.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(int argc, char** arg, char** envp) {
+	
+	// Declare an integer to hold input from stdin	
+	int val;
+
+	// Read input until the EOF token or non-integer
+	while(scanf("%d", &val) != EOF) {
+
+		// Check if the value is divisible by 3
+		if (val % 3 == 0) {
+
+			// Check if the value is also divisible by 5
+			if (val % 5 == 0) {
+
+				// Print out "Boomer Sooner!" if the value is divisible by both 3 and 5
+				printf("Boomer Sooner!\n");
+			}
+
+			else {
+				// Print out "Boomer!" if the value is only divisible by 3
+				printf("Boomer!\n");
+			}
+		}
+
+		// Check if the value is divisible by 5
+		else if (val % 5 == 0) {
+			// Print out "Sooner!" if the value is divisible by only 5
+			printf("Sooner!\n");
+		}
+		else {
+			// If the value is not divisble by either 3 or 5 print out the value
+			printf("%d\n", val);
+		}
+
+	}	
+	// Default return
+	return 0;
+}
